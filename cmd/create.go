1c53d126d2043760a4bcf80390ef73efb263bcae
Lorenzo Drumond <drumondlorenzo@posteo.net>
2025-01-21 19:23:19 +0000

    feat: added status for when creating environments

--- cmd/create.go
+++ cmd/create.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 
 	venv "github.com/azr4e1/venv-notary"
+	"github.com/azr4e1/venv-notary/graphics"
 	"github.com/spf13/cobra"
 )
 
@@ -11,34 +12,38 @@ var (
 	createCmd = &cobra.Command{
 		Use:               "create",
 		Short:             "Create a local or global virtual environment",
-		RunE:              createCobraFunc,
+		RunE:              graphics.StatusMain("Creating environment...", "Environment created.", createAction),
 		Args:              cobra.MaximumNArgs(1),
 		ValidArgsFunction: venvCompletion,
 	}
 )
 
-func createCobraFunc(cmd *cobra.Command, args []string) error {
-	if len(args) > 0 && localVenv {
-		return errors.New("you cannot create a global venv and a local venv at the same time.")
-	}
-	notary, err := venv.NewNotary()
-	if err != nil {
-		return err
-	}
-	if localVenv {
-		err = notary.CreateLocal(pythonVersion)
+// func createCobraFunc(cmd *cobra.Command, args []string) error {
+
+func createAction(cmd *cobra.Command, args []string) func() error {
+	return func() error {
+		if len(args) > 0 && localVenv {
+			return errors.New("you cannot create a global venv and a local venv at the same time.")
+		}
+		notary, err := venv.NewNotary()
 		if err != nil {
 			return err
 		}
-	} else if len(args) > 0 {
-		err = notary.CreateGlobal(args[0], pythonVersion)
-		if err != nil {
-			return err
+		if localVenv {
+			err = notary.CreateLocal(pythonVersion)
+			if err != nil {
+				return err
+			}
+		} else if len(args) > 0 {
+			err = notary.CreateGlobal(args[0], pythonVersion)
+			if err != nil {
+				return err
+			}
+		} else {
+			return errors.New("you need to either create a local or global venv.")
 		}
-	} else {
-		return errors.New("you need to either create a local or global venv.")
+		return nil
 	}
-	return nil
 }
 
 func init() {
---
+++ graphics/statusline.go
@@ -0,0 +1,87 @@
+package graphics
+
+import (
+	"fmt"
+
+	"github.com/charmbracelet/bubbles/spinner"
+	tea "github.com/charmbracelet/bubbletea"
+	lg "github.com/charmbracelet/lipgloss"
+	"github.com/spf13/cobra"
+)
+
+type errMsg error
+type doneMsg int
+type actionFunc func(*cobra.Command, []string) func() error
+
+type StatusModel struct {
+	spinner        spinner.Model
+	waitingMessage string
+	exitMessage    string
+	errorMessage   string
+	errorStyle     lg.Style
+	quitting       bool
+	action         func() error
+}
+
+func (sm StatusModel) Init() tea.Cmd {
+	cobraCmd := func() tea.Msg {
+		err := sm.action()
+		if err != nil {
+			return errMsg(err)
+		}
+		return doneMsg(0)
+	}
+	cmds := []tea.Cmd{sm.spinner.Tick, cobraCmd}
+	return tea.Batch(cmds...)
+}
+
+func (sm StatusModel) View() string {
+	if sm.errorMessage != "" {
+		return errorStyle.Render("\n " + sm.errorMessage + "\n")
+	}
+	str := fmt.Sprintf("\n %s %s\n", sm.spinner.View(), sm.waitingMessage)
+	if sm.quitting {
+		return "\n " + sm.exitMessage + "\n"
+	}
+	return str
+}
+
+func (sm StatusModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case errMsg:
+		if msg != nil {
+			sm.errorMessage = msg.Error()
+		}
+		sm.quitting = true
+		return sm, tea.Quit
+	case doneMsg:
+		sm.quitting = true
+		return sm, tea.Quit
+	default:
+		var cmd tea.Cmd
+		sm.spinner, cmd = sm.spinner.Update(msg)
+		return sm, cmd
+	}
+}
+
+func newStatus(waitingMessage, exitMessage string, action func() error) StatusModel {
+	s := spinner.New()
+	s.Spinner = spinner.Dot
+	sm := StatusModel{
+		waitingMessage: waitingMessage,
+		exitMessage:    exitMessage,
+		action:         action,
+		spinner:        s,
+	}
+	return sm
+}
+
+func StatusMain(waitingMessage, exitMessage string, action actionFunc) cobraFunc {
+	return func(cmd *cobra.Command, args []string) error {
+		m := newStatus(waitingMessage, exitMessage, action(cmd, args))
+		p := tea.NewProgram(m)
+		_, err := p.Run()
+
+		return err
+	}
+}
