98f30583b22c9602a0419683c860e639a3580725
Lorenzo Drumond <drumondlorenzo@posteo.net>
2025-01-22 16:39:40 +0000

    feat: added delete status

--- cmd/delete.go
+++ cmd/delete.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 
 	venv "github.com/azr4e1/venv-notary"
+	"github.com/azr4e1/venv-notary/graphics"
 	"github.com/spf13/cobra"
 )
 
@@ -11,34 +12,36 @@ var (
 	deleteCmd = &cobra.Command{
 		Use:               "delete",
 		Short:             "Delete a local or global virtual environment",
-		RunE:              deleteCobraFunc,
+		RunE:              graphics.StatusMain("Deleting environment...", "Environment successfully deleted.", deleteAction),
 		Args:              cobra.MaximumNArgs(1),
 		ValidArgsFunction: venvCompletion,
 	}
 )
 
-func deleteCobraFunc(cmd *cobra.Command, args []string) error {
-	if len(args) > 0 && localVenv {
-		return errors.New("you cannot delete a global venv and a local venv at the same time.")
-	}
-	notary, err := venv.NewNotary()
-	if err != nil {
-		return err
-	}
-	if localVenv {
-		err = notary.DeleteLocal(pythonVersion)
+func deleteAction(cmd *cobra.Command, args []string) func() error {
+	return func() error {
+		if len(args) > 0 && localVenv {
+			return errors.New("you cannot delete a global venv and a local venv at the same time.")
+		}
+		notary, err := venv.NewNotary()
 		if err != nil {
 			return err
 		}
-	} else if len(args) > 0 {
-		err = notary.DeleteGlobal(args[0], pythonVersion)
-		if err != nil {
-			return err
+		if localVenv {
+			err = notary.DeleteLocal(pythonVersion)
+			if err != nil {
+				return err
+			}
+		} else if len(args) > 0 {
+			err = notary.DeleteGlobal(args[0], pythonVersion)
+			if err != nil {
+				return err
+			}
+		} else {
+			return errors.New("you need to either delete a local or global venv.")
 		}
-	} else {
-		return errors.New("you need to either delete a local or global venv.")
+		return nil
 	}
-	return nil
 }
 
 func init() {
