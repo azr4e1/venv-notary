12fcff1223a4465984179238654cd6fb48734aa6
Lorenzo Drumond <drumondlorenzo@posteo.net>
2025-01-29 15:03:23 +0000

    feat: use go-ps to detect shell in a os diagnostic way

--- shell/utils.go
+++ shell/utils.go
@@ -2,11 +2,9 @@ package shell
 
 import (
 	"errors"
-	"fmt"
+	ps "github.com/mitchellh/go-ps"
 	"os"
 	"os/exec"
-	"path/filepath"
-	"runtime"
 )
 
 var shellVariables = map[shellType]string{
@@ -26,34 +24,16 @@ func hasShell(shellName shellType) bool {
 }
 
 func getShellName(availableShells []shellType) (shellType, error) {
-	procName, err := getProcessName()
+	ppid := os.Getppid()
+	proc, err := ps.FindProcess(ppid)
 	if err != nil {
 		return "", err
 	}
 
 	for _, sh := range availableShells {
-		if sh == shellType(procName) {
+		if sh == shellType(proc.Executable()) {
 			return sh, nil
 		}
 	}
 	return "", errors.New("couldn't detect current shell among the supported ones")
-}
-
-func getProcessName() (string, error) {
-	ppid := os.Getppid()
-	var exePath string
-	switch runtime.GOOS {
-	case "linux":
-		if path, err := os.Readlink(fmt.Sprintf("/proc/%d/exe", ppid)); err == nil {
-			exePath = path
-		}
-	}
-
-	if exePath == "" {
-		return "", errors.New("couldn't detect parent process")
-	}
-
-	shellName := filepath.Base(exePath)
-
-	return shellName, nil
 }
